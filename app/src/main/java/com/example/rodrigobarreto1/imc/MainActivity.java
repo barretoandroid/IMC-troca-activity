d2442814cfbeb5f182d9e52bb565f10a68a6a0ae
barretorodrigo <barretorodrigo@gmail.com>
2019-07-24 22:03:23 -0300

    Aula 24/07

--- app/src/main/java/com/example/rodrigobarreto1/imc/MainActivity.java
+++ app/src/main/java/com/example/rodrigobarreto1/imc/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.rodrigobarreto1.imc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,33 @@ public class MainActivity extends AppCompatActivity {
         //Vinculação dos objetos JAVA com os elementos do Layout pelo ID
         editTextAltura = findViewById(R.id.etAltura);
         editTextPeso = findViewById(R.id.etPeso);
+    }
+
+    //Exibe o menu_principal na barra superior
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.itemSobre){
+            //Ação ao clicar sobre o item menu
+            Intent intent = new Intent(this, SobreActivity.class);
+            startActivity(intent);
+
+        }else if(id == R.id.itemConfig){
+            //Ação ao clicar configurações
+            Toast toast = Toast
+                    .makeText(getApplicationContext()
+                            , "Configurações"
+                            , Toast.LENGTH_LONG);
+            toast.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void calcular(View view){
